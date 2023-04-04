d2a8f1fb36ee1b11f76a515a70495c89f86fff77
campjake <campbell.jakelog@gmail.com>
2023-04-04 12:15:56 -0700

    Just continuing on after losing work and getting booted

---
+++ lab18.s
@@ -0,0 +1,14 @@
+// Style Sheet
+// Programmer   : Jacob Campbell
+// Lab #        : 18
+// Purpose      : IO Read Array
+// Date         : 4/6/2023
+
+    .data
+str1:  .asciz "The Cat in the Hat\n"
+str2:  .asciz "\n"
+str3:  .asciz  "By Dr. Seuss\n"
+str4:  .asciz  "\n"
+str5:  .asciz "The sun did not shine.\n"
+headPtr:  .quad  0
+tailPtr:  .quad  0
